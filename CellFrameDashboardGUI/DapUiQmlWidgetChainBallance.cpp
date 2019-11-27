f7c53e22687ccc887c0f80c1adab80110663dd93
evgenii.tagiltsev <tagiltsev.evgenii@gmail.com>
2019-11-27 16:57:02 +0100

    [*] changed structure of project

--- CellFrameDashboardGUI/DapUiQmlWidgetChainBallance.cpp
+++
@@ -1,6 +0,0 @@
-#include "DapUiQmlWidgetChainBallance.h"
-
-DapUiQmlWidgetChainBallance::DapUiQmlWidgetChainBallance(const QString &name, const QString &URLpage, const QString &image) : DapUiQmlWidget(name, URLpage, image)
-{
-    
-}
