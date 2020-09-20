c87b1a3c4a387e6788b005255a6020fb1eb3f441
alexandr.kravchenko <alexander.kravchenko@demlabs.net>
2020-09-20 13:19:49 +0000

    Moved out prod_build to separate submodule.

--- prod_build/linux/debian/scripts/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-make distclean
-rm -r prod_build/linux/debian/essentials/.debhelper/
-rm prod_build/linux/debian/essentials/cellframe-dashboard.debhelper.log
-rm prod_build/linux/debian/essentials/cellframe-dashboard.substvars
-rm -r prod_build/linux/debian/essentials/cellframe-dashboard/
-rm prod_build/linux/debian/essentials/files
