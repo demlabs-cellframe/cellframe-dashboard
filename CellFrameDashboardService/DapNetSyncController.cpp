1e6bb5d3837381ddedb98ad9da1ad63fdccc531d
Denis Sumin <denis.smolov@demlabs.net>
2023-04-03 09:26:42 +0000

    Update CellFrameDashboardService/DapNetSyncController.cpp

--- CellFrameDashboardService/DapNetSyncController.cpp
+++ CellFrameDashboardService/DapNetSyncController.cpp
@@ -29,7 +29,7 @@ QStringList DapNetSyncController::getNetworkList()
     QProcess process;
     QString command = QString("%1 net list").arg(CLI_PATH);
     process.start(command);
-    process.waitForFinished(-1);
+    process.waitForFinished(1000);
     QString result = QString::fromLatin1(process.readAll());
 
     QStringList list;
@@ -52,7 +52,7 @@ void DapNetSyncController::goSyncNet(QString net)
     QProcess process;
     QString command(QString("%1 net -net %2 go sync").arg(CLI_PATH).arg(net));
     process.start(command);
-    process.waitForFinished(10);
+    process.waitForFinished(1000);
     QString result = QString::fromLatin1(process.readAll());
     qInfo() << "result:" << result;
 }
