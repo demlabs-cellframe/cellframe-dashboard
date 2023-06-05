522a999984383f14d5c4bcc6e8c06c98df45f98a
denis.sumin <denis.sumin@demlabs.net>
2023-06-05 10:32:49 +0300

    ...

--- CellFrameDashboardGUI/DapApplication.h
+++ CellFrameDashboardGUI/DapApplication.h
@@ -13,7 +13,6 @@
 #include "mobile/testcontroller.h"
 #include "Autocomplete/CommandCmdController.h"
 #include "DapMath.h"
-#include "HistoryWorker/historyworker.h"
 #include "DiagnosticWorker/models/NodeModel.h"
 
 #include "DapLogger.h"
@@ -77,7 +76,6 @@ private:
     DiagnosticWorker *m_diagnosticWorker;
 
     StockDataWorker *stockDataWorker;
-    HistoryWorker * m_historyWorker;
     ConfigWorker *configWorker;
     StringWorker *stringWorker;
     DateWorker   *dateWorker;
